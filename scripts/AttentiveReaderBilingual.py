added adversarial fixed softmax

scripts/AttentiveReaderBilingual.py
@@ -2,12 +2,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Function
 from torch.nn import init
 from torch.nn.utils.rnn import pack_padded_sequence
 
 
-class AttentiveReader(nn.Module):
+class AttentiveReaderBilingual(nn.Module):
     """Attentive Reader"""
     def __init__(self, var_size, story_max_len, question_max_len, emb_vectors,
                  hidden_size=128,
@@ -24,7 +23,7 @@ class AttentiveReader(nn.Module):
                  discriminator_weight=0.5,
                  opt=None,
                  adv_opt=None):
-        super(AttentiveReader, self).__init__()
+        super(AttentiveReaderBilingual, self).__init__()
 
         # setting attributes
         self._var_size = var_size
@@ -45,11 +44,12 @@ class AttentiveReader(nn.Module):
         self._discriminator_weight = discriminator_weight
         self.optimiser = opt
         self.adv_optimiser = adv_opt
+        self.bypass_softmax = False
 
         # create layers
 
         # variable embeddings
-        assert(self._l1_emb_vector.shape[1] == self._l2_emb_vector)
+        assert(self._l1_emb_vector.shape[1] == self._l2_emb_vector.shape[1])
         self._l1_embedding_layer = nn.Embedding(self._l1_emb_vector.shape[0], self._l1_emb_vector.shape[1], 0)
         self._l2_embedding_layer = nn.Embedding(self._l2_emb_vector.shape[0], self._l2_emb_vector.shape[1], 0)
 
@@ -124,12 +124,12 @@ class AttentiveReader(nn.Module):
         lang, story, question, story_len, question_len = batch
         batch_size = story.size()[0]
 
-        # s_emb = self._embedding_projection_layer(self._dropout(self._embedding_layer(story)))
-        s_emb = self._dropout(self._l1_embedding_layer(story) * lang + self._l2_embedding_layer(story) * (1 - lang))
+        lang = lang.unsqueeze(1)
+        s_emb = self._dropout(self._l1_embedding_layer(story * (1 - lang))
+                              + self._l2_embedding_layer(story * lang))
 
-        # q_emb = self._embedding_projection_layer(self._dropout(self._embedding_layer(question)))
-        q_emb = self._dropout(self._l1_embedding_layer(question) * lang
-                              + self._l2_embedding_layer(question) * (1 - lang))
+        q_emb = self._dropout(self._l1_embedding_layer(question * (1 - lang))
+                              + self._l2_embedding_layer(question * lang))
 
         if self._pack:
             # TODO: still have to sort questions by length after mixing two languages
@@ -146,14 +146,17 @@ class AttentiveReader(nn.Module):
                                                             self._hidden_size * 2))  # batched matrix
         ms = ms.bmm(q_hn)  # batched [col, col, col, ...] -> batched [scalar, scalar, scalar, ...]
 
-        ss = F.softmax(ms)
+        if self.bypass_softmax:
+            ss = ms.squeeze()
+        else:
+            ss = F.softmax(ms.squeeze(), dim=1)
 
-        r = torch.sum(y_out * ss.expand_as(y_out), dim=1, keepdim=True)  # batch * 2hidden_size
+        r = torch.sum(y_out * ss.unsqueeze(2), dim=1)  # batch * 2hidden_size
 
-        out = self._output_layer(r.squeeze())
+        out = self._output_layer(r)
         # TODO: try different locations to insert adversarial network
         if self._adversarial:
-            discriminator_out = self._discriminator(ms)
+            discriminator_out = self._discriminator(r)
             return out, discriminator_out
         else:
             return out, None
@@ -165,10 +168,11 @@ class AttentiveReader(nn.Module):
         # TODO: use adversarial loss if enabled
         if self._adversarial:
             lang = batch[0]
-            # TODO: maximin
-            loss = nn.CrossEntropyLoss()(out, answers)\
-                - self._discriminator_weight * nn.BCEWithLogitsLoss()(discriminator_out, lang)
-            pass
+            # loss = nn.CrossEntropyLoss()(out, answers)\
+            #     - self._discriminator_weight * nn.BCEWithLogitsLoss()(discriminator_out, lang.unsqueeze(1))
+            answerer_loss = nn.CrossEntropyLoss()(out, answers)
+            discriminator_loss = nn.BCEWithLogitsLoss()(discriminator_out, lang.float().unsqueeze(1))
+            loss = answerer_loss - self._discriminator_weight * discriminator_loss
         else:
             loss = nn.CrossEntropyLoss()(out, answers)
         self.zero_grad()
@@ -180,7 +184,11 @@ class AttentiveReader(nn.Module):
             for p in self._discriminator.parameters():
                 p._grad = -p._grad
         self.optimiser.step()
-        return loss.data, F.softmax(out)
+        if self._adversarial:
+            return loss.data.cpu().numpy()[0], F.softmax(out, dim=1),\
+                   answerer_loss.data.cpu().numpy()[0], discriminator_loss.data.cpu().numpy()[0]
+        else:
+            return loss.data.cpu().numpy()[0], F.softmax(out, dim=1), loss.data.cpu().numpy()[0], 0
 
     def predict(self, batch):
         """call net.eval() before calling this method!"""

scripts/AttentiveReaderPlus.py
@@ -36,6 +36,7 @@ class AttentiveReader(nn.Module):
         self._emb_trainable = emb_trainable
         self._story_rec_layers = story_rec_layers
         self.optimiser = opt
+        # self.bypass_softmax = False
 
         # create layers
 
@@ -113,10 +114,13 @@ class AttentiveReader(nn.Module):
                                                             self._hidden_size * 2))  # batched matrix
         ms = ms.bmm(q_hn)  # batched [col, col, col, ...] -> batched [scalar, scalar, scalar, ...]
 
-        ss = F.softmax(ms)
+        # if self.bypass_softmax:
+        #     ss = ms.squeeze()
+        # else:
+        ss = F.softmax(ms.squeeze(), dim=1)
 
-        r = torch.sum(y_out * ss.expand_as(y_out), dim=1, keepdim=True)  # batch * 2hidden_size
-        out = self._output_layer(r.squeeze())
+        r = torch.sum(y_out * ss.unsqueeze(2), dim=1)  # batch * 2hidden_size
+        out = self._output_layer(r)
         return out
 
     def train_on_batch(self, batch):
@@ -129,12 +133,12 @@ class AttentiveReader(nn.Module):
         self._reset_nil_gradients()
         torch.nn.utils.clip_grad_norm(self.parameters(), self._max_grad_norm)
         self.optimiser.step()
-        return loss.data, F.softmax(out)
+        return loss.data, F.softmax(out, dim=1)
 
     def predict(self, batch):
         """call net.eval() before calling this method!"""
         out = self.forward(batch)
-        return F.softmax(out)
+        return F.softmax(out, dim=1)
 
     def _reset_nil_gradients(self):
         if self._emb_trainable:

scripts/data_utils.py
@@ -81,23 +81,56 @@ class QADataset(tud.Dataset):
         return s, q, s_len, q_len, s_var, q_var, answer
 
 
-# def get_embeddings(vocab, nr_unk=100, nr_var=600):
-#     # nr_vector = max(lex.rank for lex in vocab) + 1
-#     nr_vector = np.sum([x.has_vector for x in vocab])
-#     vectors = np.zeros((nr_vector + nr_unk + nr_var + 2, vocab.vectors_length), dtype='float32')
-#     dic = dict()
-#     i = 0
-#     np.seterr(all='raise')
-#     for lex in vocab:
-#         if lex.has_vector:
-#             try:
-#                 vectors[i + nr_unk + nr_var + 2, :] = lex.vector / lex.vector_norm
-#             except FloatingPointError:
-#                 # TODO: SpaCy bug that introduces new erroneous vectors that should not exist
-#                 pass
-#             dic[i] = lex.rank
-#             i += 1
-#     return vectors, dic
+class BiQADataset(tud.Dataset):
+    def __init__(self, data_df_1, data_df_2, nlp_1, nlp_2, rev_dic_1, rev_dic_2, relabel=True, l2_supersample=5):
+        self.data_df_1 = data_df_1
+        self.data_df_2 = data_df_2
+        self.nlp_1 = nlp_1
+        self.nlp_2 = nlp_2
+        self.rev_dic_1 = rev_dic_1
+        self.rev_dic_2 = rev_dic_2
+        self.relabel = relabel
+        self.l2_supersample = l2_supersample
+
+    def __len__(self):
+        return self.data_df_1.shape[0] + self.data_df_2.shape[0] * self.l2_supersample
+
+    def __getitem__(self, i):
+
+        if i < self.data_df_1.shape[0]:
+            story = self.nlp_1(self.data_df_1['story'].iloc[i].lower(), parse=False, tag=False, entity=False)
+            s, s_var, ent_dict = get_word_ids(story, max_length=2000, rev_dic=self.rev_dic_1, relabel=self.relabel)
+            s_len = np.sum(s != 0)
+
+            question = self.nlp_1(self.data_df_1['question'].iloc[i].lower(), parse=False, tag=False, entity=False)
+            q, q_var, ent_dict = get_word_ids(question, max_length=50, rev_dic=self.rev_dic_1, relabel=self.relabel,
+                                              ent_dict=ent_dict)
+            q_len = np.sum(q != 0)
+
+            if self.relabel:
+                answer = ent_dict[int(re.search(r'\d+', self.data_df_1['answer'].iloc[i]).group(0))]
+            else:
+                answer = int(re.search(r'\d+', self.data_df_1['answer'].iloc[i]).group(0))
+
+            return 0, s, q, s_len, q_len, s_var, q_var, answer
+
+        else:
+            i = (i - self.data_df_1.shape[0]) % self.data_df_2.shape[0]
+            story = self.nlp_2(self.data_df_2['story'].iloc[i].lower(), parse=False, tag=False, entity=False)
+            s, s_var, ent_dict = get_word_ids(story, max_length=2000, rev_dic=self.rev_dic_2, relabel=self.relabel)
+            s_len = np.sum(s != 0)
+
+            question = self.nlp_2(self.data_df_2['question'].iloc[i].lower(), parse=False, tag=False, entity=False)
+            q, q_var, ent_dict = get_word_ids(question, max_length=50, rev_dic=self.rev_dic_2, relabel=self.relabel,
+                                              ent_dict=ent_dict)
+            q_len = np.sum(q != 0)
+
+            if self.relabel:
+                answer = ent_dict[int(re.search(r'\d+', self.data_df_2['answer'].iloc[i]).group(0))]
+            else:
+                answer = int(re.search(r'\d+', self.data_df_2['answer'].iloc[i]).group(0))
+
+            return 1, s, q, s_len, q_len, s_var, q_var, answer
 
 
 def get_embeddings(f, nr_unk=100, nr_var=600, meta=None):

scripts/data_utils_test.py
@@ -1,31 +1,42 @@
 import pandas as pd
 import numpy as np
 import torch.utils.data as tud
-from data_utils import QADataset, get_embeddings
+from data_utils import QADataset, get_embeddings, BiQADataset
 import spacy
 
-lang = 'es'
-nlp = spacy.load(lang, vectors=False)
-with open("../bilingual_vector/original/wiki.{0}.small.vec".format(lang), "r") as f:
-    emb, dic, rev_dic = get_embeddings(f, nr_unk=100, nr_var=600)
+lang1 = 'en'
+lang2 = 'es'
+nlp1 = spacy.load(lang1, vectors=False)
+nlp2 = spacy.load(lang2, vectors=False)
+with open("../bilingual_vector/original/wiki.{0}.small.vec".format(lang1), "r") as f:
+    emb1, dic1, rev_dic1 = get_embeddings(f, nr_unk=100, nr_var=600)
 print("embedding loaded!")
-train = pd.read_pickle("../input_data/train_{0}.pkl".format(lang))
-ds = QADataset(train, nlp, rev_dic, True)
+with open("../bilingual_vector/original/wiki.{0}.small.vec".format(lang2), "r") as f:
+    emb2, dic2, rev_dic2 = get_embeddings(f, nr_unk=100, nr_var=600)
+print("embedding loaded!")
+train1 = pd.read_pickle("../input_data/train_{0}.pkl".format(lang1))
+print(train1.shape)
+train2 = pd.read_pickle("../input_data/train_{0}.pkl".format(lang2))
+print(train2.shape)
+ds = BiQADataset(train1, train2, nlp1, nlp2,  rev_dic1, rev_dic2, True, 5)
 print(ds.__len__())
-s, q, sl, ql, sv, qv, a = ds.__getitem__(1)
-print(s.shape)
-print(q)
-print(a)
-print(sl)
-print(ql)
-print(sv.shape)
-print(qv.shape)
+# ln, s, q, sl, ql, sv, qv, a = ds.__getitem__(1)
+# print(ln)
+# print(s.shape)
+# print(q)
+# print(a)
+# print(sl)
+# print(ql)
+# print(sv.shape)
+# print(qv.shape)
+print('-' * 20)
 
-qa_loader = tud.DataLoader(ds, batch_size=32, pin_memory=True, num_workers=3)
-# s, q, sl, ql, sv, qv, a = next(iter(qa_loader))
+qa_loader = tud.DataLoader(ds, batch_size=32, pin_memory=True, num_workers=3, shuffle=True)
+# ln, s, q, sl, ql, sv, qv, a = next(iter(qa_loader))
+# print(ln)
 # print(a)
 # print(s.shape)
 # print(q.shape)
-#
+
 for i, qa in enumerate(qa_loader):
     print("batch {0} loaded!".format(i))